6b467d05e16030f4a848b0797281c61152e460d0
Daniel Marin <A01247291@tec.mx>
2025-09-17 19:49:54 -0600

    Reorganización y nuevo archivo
    Se reorganizaron los archivos con carpetas para un mejor orden. Se añadió el archivo "Act2_pt5.py".

---
+++ Actividad 2/Herramientas para visión - Actividad 2/Act2_pt5.py
@@ -0,0 +1,30 @@
+import cv2
+
+cam_port = 0
+cam = cv2.VideoCapture(cam_port)
+
+while True:
+
+    result, frame = cam.read()
+
+      
+    
+    if result: 
+        
+        # Convertimos cada cuadro a escala de grises
+        gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Deteccion de bordes Canny en cada cuadro 
+        edges = cv2.Canny(gris, 100, 200)
+
+        cv2.imshow("Presiona 'q' para salir", edges)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    else:
+        print("No image detected")
+        break
+
+cam.release()
+cv2.destroyAllWindows()
